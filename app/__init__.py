c33ee0f85b0f39371cc0d633e704422e81f10159
Earl Gwin <egwin@wgu.edu>
2025-05-31 19:06:25 -0400

    virtual environment created

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
